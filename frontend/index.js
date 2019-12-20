555bb1e16239fab472377fd8fc900fec62d210c1
EmigrantDolphin <liutas58@gmail.com>
2019-12-20 19:59:14 +0200

    changed var names

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 
 var selectedComboBoxIndex = 0;
-var json = [];
+var companyData = []; // [{tableName, tableRows[]}]
 const contentDiv = document.getElementById("contentDiv");
 const downloadButtonDiv = document.getElementById("downloadButtonDiv");
 const tableComboBoxDiv = document.getElementById("tableComboBoxDiv");
@@ -15,9 +15,9 @@ async function fetchData(){
             'Content-Type': 'application/json'
         }
     });
-    json = await response.json(); // [{tableName, tableRows[]}]
+    companyData = await response.json(); // [{tableName, tableRows[]}]
     
-    if (json.length === 0){
+    if (companyData.length === 0){
         contentDiv.innerHTML = "Įmonė nerasta";
         downloadButtonDiv.innerHTML = "";
         tableComboBoxDiv.innerHTML = "";
@@ -34,12 +34,12 @@ function fillCompanyContent(){
     contentDiv.innerHTML = "";
     
     //array of arrays of objects. [tables][{tableName, tableRows[]}]
-    for (let j = 0; j < json[selectedComboBoxIndex].tableRows.length; j++){
+    for (let j = 0; j < companyData[selectedComboBoxIndex].tableRows.length; j++){
         contentDiv.innerHTML +=`
         <div>
             ${
-                Object.keys(json[selectedComboBoxIndex].tableRows[j]).map((key)=>{
-                    return "<div>"+key +": " + json[selectedComboBoxIndex].tableRows[j][key] + ". </div>"
+                Object.keys(companyData[selectedComboBoxIndex].tableRows[j]).map((key)=>{
+                    return "<div>"+key +": " + companyData[selectedComboBoxIndex].tableRows[j][key] + ". </div>"
                 }).join('')
             }
         </div>
@@ -54,23 +54,22 @@ function createDownloadButton(){
     `;
 }
 
-function downloadCsv(){
-    
+function downloadCsv(){   
     let outputData = "";
-    Object.keys(json[selectedComboBoxIndex].tableRows[0]).forEach((key)=>{ //csv header
+    Object.keys(companyData[selectedComboBoxIndex].tableRows[0]).forEach((key)=>{ //csv header
         outputData += key + ";";
     });
 
-    for (let i = 0; i < json[selectedComboBoxIndex].tableRows.length; i++){ //csv contents
+    for (let i = 0; i < companyData[selectedComboBoxIndex].tableRows.length; i++){ //csv contents
         outputData += "\n";
-        Object.keys(json[selectedComboBoxIndex].tableRows[i]).forEach((key)=>{
-            outputData += json[selectedComboBoxIndex].tableRows[i][key] + ";";
+        Object.keys(companyData[selectedComboBoxIndex].tableRows[i]).forEach((key)=>{
+            outputData += companyData[selectedComboBoxIndex].tableRows[i][key] + ";";
         });
     }
 
     const element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(outputData));
-	element.setAttribute('download', `${json[selectedComboBoxIndex].tableName}.csv`);
+	element.setAttribute('download', `${companyData[selectedComboBoxIndex].tableName}.csv`);
 
 	element.style.display = 'none';
 	document.body.appendChild(element);
@@ -91,18 +90,15 @@ async function onCompanyTextFieldKeyUp(){
             'Content-Type': 'application/json'
         }
     });
-    const respJson = await response.json();
+    const codePrediction = await response.json();
     const codePredictionDiv = document.getElementById("codePredictionDiv");
-    if (respJson.length > 0){
+    if (codePrediction.length > 0){
         codePredictionDiv.innerHTML = "Galimas įmonės kodas: " + 
-            Object.keys(respJson[0]).map((key)=>{
-                return respJson[0][key];
+            Object.keys(codePrediction[0]).map((key)=>{
+                return codePrediction[0][key];
             });
-    }else{
-        codePredictionDiv.innerHTML = "";
-        console.log("empty");
-    }
-    
+    }else
+        codePredictionDiv.innerHTML = "Galimas įmonės kodas nerastas";
 }
 
 function onTableComboBoxChange(){
@@ -113,7 +109,7 @@ function onTableComboBoxChange(){
 
 function createTableComboBox(){
     let texts = [];
-    json.forEach((table)=>{texts.push(table.tableName)});
+    companyData.forEach((table)=>{texts.push(table.tableName)});
 
     tableComboBoxDiv.innerHTML = 
     `
@@ -121,5 +117,4 @@ function createTableComboBox(){
         ${texts.map((text, index)=>{return `<option value="${index}">${text}</option>`})}
     </select>
     `;
-    
 }
